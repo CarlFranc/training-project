e91ae29dd89a43cc0b19c5cbbfaf4666d58ca871
Joey Francisco <joeycarf@icloud.com>
2026-06-17 19:06:50 +0800

    refactored package name

--- src/main/java/com/personal/project/PersonalProjectApplication.java
+++ src/main/java/com/personal/project/PersonalProjectApplication.java
@@ -1,4 +1,4 @@
-package com.personal.personalproject;
+package com.personal.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/personal/project/PersonalProjectApplicationTests.java
+++ src/test/java/com/personal/project/PersonalProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.personal.personalproject;
+package com.personal.project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

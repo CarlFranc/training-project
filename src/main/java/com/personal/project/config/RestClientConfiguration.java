99097ee69c38499752483b60137119e29cea7a58
Joey Francisco <joeycarf@icloud.com>
2026-06-20 22:02:24 +0800

    Added restClient

---
+++ src/main/java/com/personal/project/config/RestClientConfiguration.java
@@ -0,0 +1,21 @@
+package com.personal.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.ApiVersionInserter;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+class RestClientConfiguration {
+
+    @Bean
+    RestClient restClient() {
+        return RestClient.builder()
+                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .defaultApiVersion("1.0")
+                .apiVersionInserter(ApiVersionInserter.useHeader("API-version"))
+                .build();
+    }
+
+}

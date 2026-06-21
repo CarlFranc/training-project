755fe5901434e76d6afecfb2377bccdb1d1ffb3b
Joey Francisco <joeycarf@icloud.com>
2026-06-21 19:21:35 +0800

    add apache httpclient, modified ControllerAdvice

--- src/main/java/com/personal/project/config/RestClientConfiguration.java
+++ src/main/java/com/personal/project/config/RestClientConfiguration.java
@@ -1,5 +1,8 @@
 package com.personal.project.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -12,8 +15,16 @@ class RestClientConfiguration {
 
     @Bean
     RestClient restClient() {
+        PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
+        poolingHttpClientConnectionManager.setMaxTotal(200);
+        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(20);
+
+        CloseableHttpClient closeableHttpClient = HttpClients.custom()
+                .setConnectionManager(poolingHttpClientConnectionManager)
+                .build();
+
         return RestClient.builder()
-                .requestFactory(new HttpComponentsClientHttpRequestFactory())
+                .requestFactory(new HttpComponentsClientHttpRequestFactory(closeableHttpClient))
                 .defaultApiVersion("1.0")
                 .apiVersionInserter(ApiVersionInserter.useHeader("API-version"))
                 .defaultHeader(HttpHeaders.ACCEPT, "application/json")
--- src/main/java/com/personal/project/controllers/advice/ExceptionControllerAdvice.java
+++ src/main/java/com/personal/project/controllers/advice/ExceptionControllerAdvice.java
@@ -1,23 +1,52 @@
 package com.personal.project.controllers.advice;
 
 import com.personal.project.pojo.errors.ErrorResponse;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.NestedExceptionUtils;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionControllerAdvice {
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<?> handle(Exception ex) {
+    static HttpHeaders defaultHeader = new HttpHeaders();
+
+    @PostConstruct
+    static void init() {
+        defaultHeader.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
+    }
+
+    @ExceptionHandler(value = HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<?> handle(HttpRequestMethodNotSupportedException ex) {
+        Throwable root = NestedExceptionUtils.getMostSpecificCause(ex);
+        log.error("HttpRequestMethodNotSupportedException Exception: ", root);
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .message(ex.getMessage())
                 .status(HttpStatus.BAD_REQUEST)
                 .timestamp(Instant.now())
                 .build();
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(errorResponse, defaultHeader,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<?> handleGenericException(Exception ex) {
+        Throwable root = NestedExceptionUtils.getMostSpecificCause(ex);
+        log.error("Root Exception: ", root);
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(ex.getMessage())
+                .status(HttpStatus.BAD_REQUEST)
+                .timestamp(Instant.now())
+                .build();
+        return new ResponseEntity<>(errorResponse, defaultHeader ,HttpStatus.BAD_REQUEST);
     }
 }
